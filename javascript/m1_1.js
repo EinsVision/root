4ebf02372ea07ccc686d4c976c024c84e295a8b7
jungchangdae <einsvision@gmail.com>
2020-12-17 23:33:59 +0900

    About DOM

--- javascript/m1_1.js
+++ javascript/m1_1.js
@@ -1,1 +1,16 @@
-alert("hello Javascript");
+var 바디 = document.body;
+var 단어 = document.createElement('div');
+word_tag.textContent = '풀이다';
+document.body.append(단어);
+
+// var word = '나무다';
+
+// while(true){
+//     var answer = prompt(word);
+//     if(word[word.length - 1] === answer[0]){
+//         alert('딩동댕');
+//     }
+//     else{
+//         alert('땡');
+//     }
+// }
