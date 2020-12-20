bb07ee610c12527ea431d8048016a2284b1b6209
jungchangdae <einsvision@gmail.com>
2020-12-20 17:15:00 +0900

    Relational operator, Logical operator

---
+++ javascript/m1_5.js
@@ -0,0 +1,14 @@
+// Relational operator, 관계연산자
+// Logical operator, 논리연산자
+var a = 1, b = 2;
+
+console.log(a>b); // false return 
+console.log(b>a); // true return
+
+console.log(a==b);
+console.log(a!=b);
+
+console.log(!a);
+
+console.log(true&&true);
+console.log(true||false);
