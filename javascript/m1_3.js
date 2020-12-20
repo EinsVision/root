7f97618c8b2f3c35c1a98df00f80efd927d29a86
jungchangdae <einsvision@gmail.com>
2020-12-20 17:06:18 +0900

    operation & function

---
+++ javascript/m1_3.js
@@ -0,0 +1,18 @@
+// 연산자
+var a = 1, b = 2;
+
+console.log(a+b);
+console.log(a++);
+console.log(b++);
+// 증감, 감소 연산자
+console.log(a);
+console.log(b);
+
+console.log(++a);
+console.log(++b);
+console.log(a);
+console.log(b);
+
+console.log("Math.pow(2,3): "+Math.pow(2,3));
+console.log("Math.sqrt(16): "+Math.sqrt(16));
+console.log("Math.random(): "+Math.random());
---
+++ javascript/m1_4.js
@@ -0,0 +1,13 @@
+// 함수의 개념
+function func1(num1, num2){
+    return num1+num2;
+}
+
+console.log(func1(3,5));
+
+function print_msg(msg){
+    console.log("Hello");
+    console.log(msg);
+}
+
+console.log(print_msg("Hello function"));
