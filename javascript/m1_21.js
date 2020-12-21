ff2ed1af43ce393de38854f8fbba57ae82b2d04f
jungchangdae <einsvision@gmail.com>
2020-12-22 01:02:27 +0900

    event handler 1

--- javascript/m1_21.js
+++ javascript/m1_21.js
@@ -1,2 +1,9 @@
 // event 종류에는 
 // form event, window event, mouse event, key event 가 있다.
+
+var t = document.getElementById("form1");
+t.onsubmit = function a(){
+    console.log("from property");
+    return false;
+}
+
