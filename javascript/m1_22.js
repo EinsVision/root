cf6b0b870fe729662e6b9b44d2f5b52c121139f8
jungchangdae <einsvision@gmail.com>
2020-12-22 17:02:52 +0900

    AJAX

---
+++ javascript/m1_22.js
@@ -0,0 +1,7 @@
+// AJAX (Asyncronous Javascript and XML)
+// 브라우저에서 페이지를 이동하지 않고 자바스크립트를 통해 
+// HTTP Request를 보내고 그 응답을 받아 처리할 수 있는 기술
+
+var req = new XMLHttpRequest();
+req.open("GET", "./data.txt");
+req.send();
