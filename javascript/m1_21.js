bcaf8979bcb2cfa65f705eef8f3a3875a0193851
jungchangdae <einsvision@gmail.com>
2020-12-22 12:02:47 +0900

    event handler 등록하는 방법

--- javascript/m1_21.js
+++ javascript/m1_21.js
@@ -1,9 +1,24 @@
 // event 종류에는 
 // form event, window event, mouse event, key event 가 있다.
 
+// event handler 등록하기
 var t = document.getElementById("form1");
 t.onsubmit = function a(){
     console.log("from property");
     return false;
 }
 
+function b(){
+    console.log("from addEventLIstener");
+    return false;
+}
+
+function c(){
+    console.log("from addEventLIstener #2");
+    return false;
+}
+
+t.addEventListener('submit',b);
+t.addEventListener('submit',c);
+
+t.removeEventListener
