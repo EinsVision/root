810c29db79c1afff85761b99e89af14faf55cfa1
jungchangdae <einsvision@gmail.com>
2021-01-04 10:24:39 +0900

    Vanila javascript data type

--- javascript/vanila.js
+++ javascript/vanila.js
@@ -1,5 +1,11 @@
-let test = 1;
+const test = 1;
 let result = 221;
 result = result - test;
-
+// test = 2; 에러를 발생시킴
 console.log(result);
+
+// vanila javascript data type
+const what = "Jungchangdae"; // string
+const bin = true;            // boolean
+const num = 77;              // number
+const floa = 0.1;            // float 
