f55bf1b13c68ace17b492365f9d773ac62c18aea
jungchangdae <einsvision@gmail.com>
2021-01-05 17:26:03 +0900

    to do list 만들기 3

--- javascript/todo.js
+++ javascript/todo.js
@@ -23,15 +23,17 @@ function saveToDos(){
 function paintToDo(text){
     const li = document.createElement('li');
     const delBtn = document.createElement('button');
-    delBtn.innerText = '✂';
+    delBtn.innerText = '❌';
     delBtn.addEventListener('click', deleteToDo);
 
     const span = document.createElement('span');
     const newID = toDos.length + 1;
     span.innerText = text;
-    li.appendChild(span);
+
     li.appendChild(delBtn);
+    li.appendChild(span);
     li.id = newID;
+    
     toDoList.appendChild(li);
     const toDoObj = {
         text: text,
