94d53d811eaa3eb396be8c911b713fcdf417e105
Changdae <einsvision@gmail.com>
2021-03-10 08:57:26 +0900

    javascript es6

---
+++ js/fc1_1.js
@@ -0,0 +1,9 @@
+// logical operator (NOT !, AND &&, OR ||)  연산자 우선순위 not and or
+const logicalOperator = true;
+console.log('logicalOperator: ', !logicalOperator);
+
+const andOperator = true && true;
+console.log('andOperator: ', andOperator);
+
+const orOperator = true && false;
+console.log('orOperator: ', orOperator);
